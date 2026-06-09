c96f3b412828caff8772b2d19eb3fef743897b90
Jack Atkinson <jwa34@cam.ac.uk>
2026-06-09 12:34:40 +0100

    Update exercise 01 Fortran code: align with FTorch v1.1.0 patterns
    - Template: hint at 'use ftorch' style (without double colon)
    - Solution: add torch_model_print_parameters call
    - Solution: add allclose-based output verification
    - Solution: update model file name to match pt2ts output naming
    - Use 'use ftorch' style consistent with library examples

--- exercises/exercise_01/simplenet_fortran_sol.f90
+++ exercises/exercise_01/simplenet_fortran_sol.f90
@@ -4,14 +4,12 @@ program inference
    use, intrinsic :: iso_fortran_env, only : sp => real32
 
    ! Import our library for interfacing with PyTorch
-   use :: ftorch, only : &
-        torch_kCPU, &
-        torch_tensor_from_array, &
-        torch_model_load, &
-        torch_model_forward, &
-        torch_delete, &
-        torch_tensor, &
-        torch_model
+   use ftorch, only : torch_model, torch_tensor, torch_kCPU, torch_delete, &
+                      torch_tensor_from_array, torch_model_load, &
+                      torch_model_forward, torch_model_print_parameters
+
+   ! Import our tools module for testing utils
+   use ftorch_test_utils, only : allclose
 
    implicit none
 
@@ -21,6 +19,7 @@ program inference
    ! Set up Fortran data structures
    real(wp), dimension(5), target :: in_data
    real(wp), dimension(5), target :: out_data
+   real(wp), dimension(5) :: expected
 
    ! Set up Torch data structures
    ! The net, a vector of input tensors, and a vector of output tensors
@@ -29,7 +28,10 @@ program inference
    type(torch_model) :: torch_net
 
    ! Set Torchscript model path (relative to the build directory)
-   character(len=128) :: model_torchscript_file = '../saved_model.pt'
+   character(len=128) :: model_torchscript_file = '../torchscript_simplenet_model_cpu.pt'
+
+   ! Flag for testing
+   logical :: test_pass
 
    ! Initialise data
    in_data = [0.0, 1.0, 2.0, 3.0, 4.0]
@@ -41,16 +43,31 @@ program inference
    ! Load ML model
    call torch_model_load(torch_net, model_torchscript_file, torch_kCPU)
 
+   ! Print the parameters associated with the pre-trained model
+   write (*,*) "Model parameters:"
+   call torch_net%print_parameters()
+
    ! Run inference on the model using `torch_model_forward`
    call torch_model_forward(torch_net, input_tensors, output_tensors)
 
    ! Write out the result of calling the net
    ! Note: data immediately available in Fortran - no need to 'map'
+   write (*,*) "Model output:"
    write (*,*) out_data(:)
+
+   ! Check output tensor matches expected value
+   expected = [0.0, 2.0, 4.0, 6.0, 8.0]
+   test_pass = allclose(out_data, expected, test_name="SimpleNet", rtol=1e-5)
 
    ! Cleanup
    call torch_delete(input_tensors)
    call torch_delete(output_tensors)
    call torch_delete(torch_net)
+
+   if (.not. test_pass) then
+     stop 999
+   end if
+
+   write (*,*) "SimpleNet example ran successfully"
 
 end program inference
